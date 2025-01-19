7f47a21089bcde5fc041a53d838a4c86ee4ce18b
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-01-19 07:19:22 -0500

    Initial Commit

---
+++ WenKa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WenKa
+//
+//  Created by Carson Mulligan on 1/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WenKa/WenKaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WenKaApp.swift
+//  WenKa
+//
+//  Created by Carson Mulligan on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WenKaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
